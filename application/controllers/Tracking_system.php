5bd5a83410ae667ed99467cb37d49861616f9945
yogesh <yogeshmittal120@gmail.com>
2020-02-05 07:20:27 +0530

    working

--- application/controllers/Tracking_system.php
+++ application/controllers/Tracking_system.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Tracking_system extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -22,4 +22,9 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+	public function login()
+	{
+		$this->load->view('welcome_message');
+	}
+
 }
